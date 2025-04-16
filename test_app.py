cf2f7c2cf2430a8ae0c6bb71f3c5af2356376051
zidan <zidan@101032330180.com>
2025-04-16 04:39:50 +0000

    empat

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import hello
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(),"Hello, world?")
+        self.assertEqual(hello(),"Hello, World!")
 if __name__ == "__main__":
     unittest.main()
