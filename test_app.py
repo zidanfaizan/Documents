8ec2efccc30607ce45416de08fa4e6d80fa3bc52
zidan <zidan@101032330180.com>
2025-04-16 04:41:24 +0000

    lma

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import hello
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(),"Hello, World!")
+        self.assertEqual(hello(),"Hello, world?")
 if __name__ == "__main__":
     unittest.main()
